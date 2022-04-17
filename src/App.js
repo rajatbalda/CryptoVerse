81fa9897ac6630efe2bb5d6dfb62d0e11b28b6b1
Rajat Balda <rajatbalda@gmail.com>
2022-04-17 22:37:45 +0100

    setting up projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from '.react-router-dom';
+import { Layout, Typography, Space } from '.antd';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Header */}
+      <div className="nav">
+
+      </div>
+
+      {/* Main */}
+      <div className="main">
+
+      </div>
+
+      {/* Footer */}
+      <div className="footer">
+
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Menu, Typographar, Avatr } from 'antd'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Navbar
